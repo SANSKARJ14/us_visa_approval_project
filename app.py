fe213772c1e15ad485e587e1f1088ddd4956d420
SANSKARJ14 <ssanskarjain14@gmail.com>
2024-06-13 10:24:05 +0530

    completed data ingestion sucessfully

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
-
-
+#from us_visa.data_access.usvisa_data import USvisaData
+#from us_visa.constants import *
+#usvisa_data = USvisaData()
+#dataframe=usvisa_data.export_collection_as_dataframe(collection_name=COLLECTION_NAME)
+#print(dataframe)
 
--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -23,7 +23,7 @@ TEST_FILE_NAME: str = "test.csv"
 """
 Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
-DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_COLLECTION_NAME: str = "VISA_DATA"
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
